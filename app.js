34c5d8f1b433c66ab0cd760a0a9ee4db8e98f2fc
xmaruwo <xmaruwo@gmail.com>
2023-06-05 19:00:15 +0900

    ログ処理追加、セッション管理処理追加

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const createError = require('http-errors');
 const express = require('express');
+const mysql = require('mysql2');
+const session = require('express-session');
+const fs = require('fs');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+// const fs = require('fs-extra');
+const cookieParser = require('cookie-parser');
+
+// ログ格納場所
+const logDirectory = path.join(__dirname, './logs');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const sampleRouter = require('./routes/sample');
 const loginRouter = require('./routes/login');
-const listRouter = require('./routes/list');
+const logoutRouter = require('./routes/logout');
+const listRouter = require('./routes/lists');
 
 const app = express();
 
@@ -22,11 +30,44 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// ディレクトリ存在確認
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+logger.accessLogStream = fs.createWriteStream(path.join(__dirname, './logs/access.log'), { flags: 'a'});
+
+app.use(logger('combined', { stream: logger.accessLogStream}));
+
+
+// セッション管理
+app.use(
+  session({
+    secret: 'my_secret_key',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+
+// ログイン情報確認
+app.use((req, res, next) => {
+  console.log(req.session);
+  if (req.session.userId === undefined) {
+    console.log('ログインしていません');
+    res.locals.userName = 'ゲスト';
+    res.locals.isLoggedIn = false;
+  } else {
+    console.log('ログインしています');
+    res.locals.userName = req.session.userName;
+    res.locals.isLoggedIn = true;
+  }
+  next();
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/sample', sampleRouter);
 app.use('/login', loginRouter);
-app.use('/list', listRouter);
+app.use('/logout', logoutRouter);
+app.use('/lists', listRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
