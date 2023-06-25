a8fa3036108f1b33134288daaec79c400c86f888
xmaruwo <xmaruwo@gmail.com>
2023-06-25 09:36:44 +0900

    model:article, userの関連性修正

--- models/article.js
+++ models/article.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      article.belongsTo(models.user, {foreignKey: 'id'})
+      article.belongsTo(models.user, {foreignKey: 'user_id'})
     }
   }
   article.init({
